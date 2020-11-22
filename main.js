aafa23b706f93ccf99209cb8fe31b199159ff5ee
Furkan <arabulfurkan@gmail.com>
2020-11-22 21:42:16 +0100

    redesign and refactor

--- main.js
+++ main.js
@@ -1,64 +1,105 @@
-let form = document.querySelector('#addForm');
-let itemList = document.querySelector('#items');
-let filter = document.querySelector('#filter')
-let empty = document.querySelector('#emptyList')
-//check if todoList empty
-let isEmpty = function(){
-    if(itemList.childElementCount==0){
-        empty.appendChild(document.createTextNode('There is nothing to-do. Please add something'));
-    }
-}
-isEmpty()
-    
+//selectors
+let todoInput = document.querySelector('.todo-input');
+let todoButton = document.querySelector('.todo-button');
+let todoList = document.querySelector('.todo-list');
+let headerInput = document.querySelector('.header-input');
+let darkModeButton = document.querySelector('.darkmodeBtn');
+let filterOption = document.querySelector('.filter-todo')
+//var fired=false;
 
-// submit event
-form.addEventListener('submit', itemAdd);
-// delete event
-itemList.addEventListener('click', itemRemove);
-// filter event
-filter.addEventListener('keyup', itemFilter);
 
-function itemAdd(e){
-    e.preventDefault();
-    //get input value
-    let newItem = document.querySelector('#item').value
-    //create new list item
-    let li = document.createElement('li');
-    //give class
-    li.className = 'list-group-item';
-    //add text node with input value
-    li.appendChild(document.createTextNode(newItem));
-    //create delete button
-    let deleteBtn = document.createElement('button');
-    //give class to button
-    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-    //add txt to button
-    deleteBtn.appendChild(document.createTextNode('x'));
-    //append delete button to list item
-    li.appendChild(deleteBtn);
-    //check if its empty and append li to the list
-    if(newItem.length > 0){
-        itemList.appendChild(li);
-        empty.remove();
-    } else{
-        alert('Please write something');
-    }
+//event listeners
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click',deleteTodo);
+todoList.addEventListener('click',checkTodo);
+darkModeButton.addEventListener('click',darkMode);
+filterOption.addEventListener('click', filterTodo)
+
+//functions
+function addTodo(event){
+    //prevent submit
+    event.preventDefault();
+    //to-do div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+    //create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    //create buttons
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //append todoDiv to ul
+    todoList.appendChild(todoDiv);
+    //empty value
+    todoInput.value ="";
 }
-function itemRemove(e){
-    if(e.target.classList.contains('delete')){
-        let li = e.target.parentElement
-        itemList.removeChild(li);
+
+// delete function
+    function deleteTodo(event){
+        
+        if(event.target.classList.contains('trash-btn')){
+                event.target.parentElement.classList.add('fall');
+                
+                event.target.parentElement.addEventListener('transitionend', function(){
+                    event.target.parentElement.remove()
+                })
+                removeLocalTodos(todo);
+        }
     }
-}
-function itemFilter(e){
-    let text = e.target.value.toLowerCase();
-    let items = itemList.querySelectorAll("li");
-    Array.from(items).forEach(function(item){
-        let itemName = item.firstChild.textContent;
-        if(itemName.toLowerCase().indexOf(text) != -1){
-            item.style.display ='block';
-        } else {
-            item.style.display ='none';
+// checkmark
+    function checkTodo(event){
+        if(event.target.classList.contains('complete-btn')){
+            event.target.parentElement.classList.add('completed');
+        }
+    }
+// filter to do
+function filterTodo(e){
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch (e.target.value) {
+            case "all":
+                todo.style.display ="flex";
+                break;
+            case "completed":
+                if(todo.classList.contains("completed")){
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
         }
     });
 }
+
+//removetype
+//    function removeType(event){
+//        if(!fired){
+//            fired=true;
+//            let deleteTxt = document.createElement('button');
+//            deleteTxt.innerHTML = '<i class="fas fa-trash"></i>';
+//            deleteTxt.classList.add('deleteTxt')
+//            headerInput.insertBefore(deleteTxt,headerInput.firstChild)
+//            console.log('1 oldu');        
+//        } else if(todoInput.value.length===0){
+//            console.log('0 oldu');
+//            deleteTxt.remove();
+//        }
+//        
+//    }
+
+function darkMode() {
+    let body = document.body;
+    body.classList.toggle("dark-mode");
+    this.classList.toggle('fa-sun');
+    todoInput.classList.toggle('darkforminput');
+    todoButton.classList.toggle('darkforminput');
+}
+
+
